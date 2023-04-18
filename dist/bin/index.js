9fbd59afaa05011efe771395b4f201ab4967b00e
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-04-18 15:12:15 +0545

    Refactor

--- dist/bin/index.js
+++ dist/bin/index.js
@@ -2,10 +2,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const process_1 = require("process");
-const index_1 = require("../index");
+const typescript_1 = require("../typescript");
 const jsonFilePath = process_1.argv[2];
 const fs = require('fs');
 const jsonData = fs.readFileSync(jsonFilePath, 'utf-8');
 const jsonObject = JSON.parse(jsonData);
-jsonObject.forEach(object => (0, index_1.createEndpointsAndModels)(object).then());
+jsonObject.forEach(object => (0, typescript_1.createTypescriptEndpointsAndModels)(object).then());
 //# sourceMappingURL=index.js.map
